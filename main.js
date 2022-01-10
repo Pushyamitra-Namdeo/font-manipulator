bc63cd2d68117c7ff458832d26f9fd786adaba8f
Pushyamitra-Namdeo <79986813+Pushyamitra-Namdeo@users.noreply.github.com>
2022-01-10 18:07:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX=0;
+noseY=0;
+rightWristx=0;
+leftWristx=0;
+difference=100;
+function setup(){
+    video= createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas= createCanvas(550,550);
+    canvas.position(600,150);
+
+    poseNet= ml5.poseNet(video, model_loaded);
+    poseNet.on('pose',got_poses);
+}
+
+function model_loaded(){
+    console.log("PoseNet model is loaded!");
+}
+
+function draw(){
+    background('#808080');
+    fill('#FFC0CB');
+    stroke('#FFC0CB');
+    textSize(difference);
+    text("Pushyamitra",noseX,noseY);
+
+}
+
+function got_poses(results){
+    if(results.length > 0){
+    console.log(results);
+    noseX= results[0].pose.nose.x;
+    noseY= results[0].pose.nose.y;
+    console.log("noseX="+ noseX,"noseY="+ noseY);
+    
+    rightWristx= results[0].pose.rightWrist.x;
+    leftWristx= results[0].pose.leftWrist.x;
+    difference= floor(leftWristx-rightWristx)
+    document.getElementById("size").innerHTML="The width and height of the square in realtime is "+difference;
+
+    }
+}
+
+
+
